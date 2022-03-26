80e27099defb1c4a8e44b65188d65f9232451069
Manuel Bosi <manuelbosi57@gmail.com>
2022-03-26 15:48:37 +0100

    create custom button widget

---
+++ lib/components/button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/config/colors.dart';
+
+class Button extends StatelessWidget {
+  final Function onPressed;
+  final dynamic child;
+  final String type;
+  final double? width;
+
+  const Button({
+    Key? key,
+    required this.type,
+    required this.onPressed,
+    required this.child,
+    this.width,
+  }) : super(key: key);
+
+  double isFullWidth(double? width) {
+    final double w = width ?? 0;
+    return w.toDouble();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all<Color?>(colors['white']),
+        splashFactory: NoSplash.splashFactory,
+        backgroundColor: MaterialStateProperty.all<Color?>(
+          type == "success" ? colors["success"] : colors["danger"],
+        ),
+        minimumSize: MaterialStateProperty.all<Size>(
+          Size(isFullWidth(width), 0),
+        ),
+        // padding: MaterialStateProperty.all<EdgeInsets>(
+        //   const EdgeInsets.fromLTRB(10.0, 3.0, 10.0, 3.0),
+        // ),
+        alignment: Alignment.center,
+      ),
+      onPressed: handleOnPressed,
+      child: child,
+    );
+  }
+
+  void handleOnPressed() {
+    onPressed();
+  }
+}
---
+++ lib/config/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// Colors
+// TODO: replace all color with Color.fromRGBO constructor
+Map<String, Color> colors = {
+  'primary': const Color(0xFF1572A1),
+  'success': const Color(0xFF47B09A),
+  'danger': const Color(0xFFC05959),
+  'blue': const Color(0xFF1C3550),
+  'blue-opacity': const Color(0x921C3550),
+  'purple': const Color(0xFFDADDFC),
+  'white': const Color(0xFFFFFFFF)
+};

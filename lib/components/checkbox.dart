3ed6fd930d760a7bcb3bad58380177ab965a386c
Manuel Bosi <manuelbosi57@gmail.com>
2022-03-26 15:51:51 +0100

    create custom checkbox widget

---
+++ lib/components/checkbox.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/config/colors.dart';
+
+class InputCheckbox extends StatefulWidget {
+  final Function onChange;
+  final bool isChecked;
+  final double size;
+  final Color _checkedBackground = colors['blue'] as Color;
+  final Color _checkedIconColor = colors['white'] as Color;
+  final Color _borderColor = colors['blue'] as Color;
+  final IconData _icon = Icons.check;
+
+  InputCheckbox({
+    Key? key,
+    required this.isChecked,
+    required this.onChange,
+    required this.size,
+  }) : super(key: key);
+
+  @override
+  _InputCheckboxState createState() => _InputCheckboxState();
+}
+
+class _InputCheckboxState extends State<InputCheckbox> {
+  bool _isSelected = false;
+
+  @override
+  void initState() {
+    _isSelected = widget.isChecked ? true : false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _isSelected = !_isSelected;
+          widget.onChange(_isSelected);
+        });
+      },
+      child: AnimatedContainer(
+        // margin: const EdgeInsets.all(4),
+        duration: const Duration(milliseconds: 100),
+        curve: _isSelected ? Curves.easeIn : Curves.easeOut,
+        decoration: BoxDecoration(
+          color: _isSelected ? widget._checkedBackground : Colors.transparent,
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(
+            color: widget._borderColor,
+            width: 1.5,
+          ),
+        ),
+        width: widget.size,
+        height: widget.size,
+        child: _isSelected
+            ? Icon(
+                widget._icon,
+                color: widget._checkedIconColor,
+                size: widget.size - 5,
+              )
+            : null,
+      ),
+    );
+  }
+}

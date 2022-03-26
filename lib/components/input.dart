c7f3b8eb9d10dd23afe6848968dfb4770dda564b
Manuel Bosi <manuelbosi57@gmail.com>
2022-03-26 17:40:53 +0100

    create custom input widget with validations

---
+++ lib/components/input.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/config/colors.dart';
+import 'package:shopping_list/extensions/string_validator.dart';
+
+class Input extends StatelessWidget {
+  final String type;
+  final TextEditingController controller;
+  final String? label;
+  final bool isRequired;
+  final int? minLength;
+  final int? maxLength;
+
+  const Input({
+    Key? key,
+    required this.type,
+    required this.controller,
+    this.label,
+    this.isRequired = false,
+    this.minLength,
+    this.maxLength,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField(builder: (context) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextFormField(
+            controller: controller,
+            obscureText: type == 'password',
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: InputDecoration(
+              errorStyle: TextStyle(
+                color: colors['red'],
+              ),
+            ),
+            validator: validateField,
+          ),
+        ],
+      );
+    });
+  }
+
+  String? validateField(String? value) {
+    // Check for empty or null value
+    if (isRequired && (value == null || value.isEmpty)) {
+      return "Campo obbligatorio";
+    }
+
+    // Validate field type
+    if (type == 'email') {
+      return value.isValidEmail() ? null : "Email non valida";
+    }
+
+    // Validate field length
+    if (minLength != null && maxLength != null) {
+      final isValid =
+          value.isGreatherThan(minLength) && value.isLowerThan(maxLength);
+      return isValid
+          ? null
+          : 'Il campo deve essere tra $minLength e $maxLength caratteri';
+    }
+    if (maxLength != null) {
+      return value.isLowerThan(maxLength)
+          ? null
+          : 'Il campo deve essere di almeno $maxLength caratteri';
+    }
+    if (minLength != null) {
+      return value.isGreatherThan(minLength)
+          ? null
+          : 'Il campo deve essere di almeno $minLength caratteri';
+    }
+
+    return null;
+  }
+}
